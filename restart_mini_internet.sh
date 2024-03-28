629f2593232ad3e814c17f0b4531de94654413a6
Ananda Fitra <anandadf@student.ub.ac.id>
2024-03-28 14:24:03 +0700

    `add`: add interactivity

--- restart_mini_internet.sh
+++ restart_mini_internet.sh
@@ -1,178 +1,224 @@
 #!/bin/bash
 
-# -------------- START OF VARIABLES BLOCK -------------- #
-user=ubuntu
-# --------------  END OF VARIABLES BLOCK  -------------- #
+USER=ubuntu
 
-change_directory() {
-        # Set working directory
-        WORKDIR=/home/${user}/mini_internet_project/platform/
-        # Change working dir
-        cd $WORKDIR
-        # Verify working dir
-        echo "Current workdir: $WORKDIR"
+WORKDIR=/home/${USER}/mini_internet_project/platform/
+students_as=(3 4 13 14)
+routers=('ZURI' 'BASE' 'GENE' 'LUGA' 'MUNI' 'LYON' 'VIEN' 'MILA')
+dchosts=('FIFA' 'UEFA')
+
+# save all configs first
+save_configs() {
+  cd $WORKDIR../
+  if ! [[ -d students_config ]]; then
+    mkdir students_config
+  fi
+
+  cd students_config/
+  rm -rf *
+
+  for as in ${students_as[@]}; do
+    echo "Saving config on AS: ${as}"
+
+    docker exec -itw /root ${as}_ssh bash -c 'rm -rfv configs*' > /dev/null
+    docker exec -itw /root ${as}_ssh "./save_configs.sh" > /dev/null
+
+    configName=$(docker exec -itw /root ${as}_ssh bash -c 'find . -maxdepth 1 -regex \./.*.tar.gz' | sed -e 's/\r$//')
+    docker exec -itw /root ${as}_ssh bash -c "mv $configName configs-as-${as}.tar.gz"
+
+    docker cp ${as}_ssh:/root/configs-as-${as}.tar.gz ./configs-as-${as}.tar.gz
+  done
 }
 
-execute_ssh_port_forward() {
-        echo "Adding IP route on host at device ssh_to_group for activate SSH"
-        ip addr add 157.0.0.1/24 dev ssh_to_group
-        ip link set ssh_to_group up
+reset_with_startup() {
+  echo "Resetting mini internet with startup.sh again..."
+  cd $WORKDIR
+  
+  # Hard reset
+  echo "Executing cleanup.sh & hard_reset.sh ..."
+  ./cleanup/cleanup.sh .
+  ./cleanup/hard_reset.sh .
 
-        echo "Running portforwarding.sh"
-        change_directory
-        ${WORKDIR}/utils/ssh/portforwarding.sh ${WORKDIR}
+  # Then startup
+  echo "Executing startup.sh ..."
+  ./startup.sh . && ./utils/ssh/portforwarding.sh . && ./utils/iptables/filters.sh .
+
+  echo "Waiting for docker container to ready first, sleeping in 3 seconds..."
+  sleep 3
+  
+  # Start MATRIX container
+  docker unpause MATRIX
 }
 
-add_vlans_l2() {
-        # Only occur for BASE, ZURI, and GENE
-        # Add VLANs to the interfaces
-        router_containers=$(docker ps -a --format {{.Names}} | grep 'router')
+restore_configs() {
+  for as in ${students_as[@]}; do
+    cd $WORKDIR../students_config/
 
-        for rc in $router_containers; do
-                as_number=$(echo $rc | cut -d '_' -f 1)
-                router_name=$(echo $rc | cut -d '_' -f 2 | sed 's/[a-z]//g')
-                if [[ ($as_number -eq 3 || $as_number -eq 4 || $as_number -eq 13 || $as_number -eq 14) ]]; then
-                        # Filter only BASE ZURI GENE
-                        if [[ ($router_name == "BASE" || $router_name == "ZURI" || $router_name == "GENE") ]]; then
-                                echo "Adding VLAN interfaces to ${rc}"
-                                for vlanId in $(seq 1 3); do
-                                        docker exec -it ${rc} ip link add link ${router_name}-L2 name ${router_name}-L2.$(( 10 * $vlanId )) type vlan id $vlanId
-                                done
-                        fi
-                fi
-        done
-}
+    echo "Restoring config on AS: ${as}"
+    docker cp ./configs-as-${as}.tar.gz ${as}_ssh:/root/configs-as-${as}.tar.gz
 
-hard_restart() {
-        if [[ $(docker ps -af "status=exited" --format {{.Names}} | wc -w) -eq 0 ]]; then
-                echo "Your mini project is fine, exiting..."
-                exit 0
+    # How to use heredoc works here?
+    docker exec -iw /root ${as}_ssh bash -c "./restore_configs.sh configs-as-${as}.tar.gz all" << EOF
+Y
+EOF
+
+    # Extract the config file
+    cd $WORKDIR../students_config/; rm -rf configs_*; tar -xf configs-as-${as}.tar.gz
+    # Get configs folder name
+    configs_folder_name=$(ls -d */ | grep configs)
+
+    # Restore router files
+    for rc in ${routers[@]}; do
+      cd $WORKDIR../students_config/
+
+      container_name=${as}_${rc}router
+
+      # Overwrite backuped router config file to the /etc/frr/frr.conf
+      echo "Restoring $container_name configuration..."
+      docker cp ${configs_folder_name}${rc}/router.conf ${container_name}:/root/frr.conf
+
+      # Remove the building configuration and current configuration text
+      docker exec -itw /root ${container_name} bash -c 'sed '1,3d' /root/frr.conf > /root/frr-removed-header.conf'
+
+      docker exec -itw /root ${container_name} bash -c '/usr/lib/frr/frr-reload.py --reload /root/frr-removed-header.conf'
+      sleep 2
+      docker exec -itw /root ${container_name} bash -c 'rm /root/{frr,frr-removed-header}.conf'
+    done
+    
+    # Restore router hosts
+    for rc in ${routers[@]}; do
+      cd $WORKDIR../students_config/
+
+      container_name=${as}_${rc}host
+
+      echo "Restoring $container_name configuration..."
+      # Get the IPv4 address
+      ipv4=$(cat ${configs_folder_name}${rc}/host.ip | grep -w inet | grep ${rc}router | awk '{print $2}')
+      echo "Backuped $container_name IPv4: ${ipv4}"
+      # Get the IPv6 address
+      ipv6=$(cat ${configs_folder_name}${rc}/host.ip | grep -w inet6 | grep ${rc}router | awk '{print $2}')
+      echo "Backuped $container_name IPv6: ${ipv6}"
+      # Get default route (IPv4 only?)
+      default_route=$(cat ${configs_folder_name}${rc}/host.route | grep -w default | awk '{print $3}')
+      echo "Backuped $container_name Default Route: ${default_route}"
+
+      # Adding the IPv4 and IPv6 address
+      docker exec -itw /root ${container_name} ip address add ${ipv4} dev ${rc}router &> /dev/null
+      docker exec -itw /root ${container_name} ip address add ${ipv6} dev ${rc}router &> /dev/null
+      docker exec -itw /root ${container_name} ip route add default via ${default_route} &> /dev/null
+    done
+    
+    # Restore switch files into switch
+    for sw in $(seq 1 4); do
+      cd $WORKDIR../students_config/
+
+      # Init switch loc
+      switch_name=S${sw}
+      data_center_loc='DCN'
+      if [[ $switch_name == 'S4' ]]; then
+        data_center_loc='DCS'
+      fi
+
+      container_name=${as}_L2_${data_center_loc}_${switch_name}
+
+      # Get configs folder name
+      configs_folder_name=$(ls -d */ | grep configs)
+
+      # Overwrite backuped switch file to the /etc/openvswitch/conf.db
+      echo "Restoring $container_name configuration..."
+      docker cp ${configs_folder_name}${switch_name}/switch.db ${container_name}:/root/switch.db
+      docker exec -itw /root ${container_name} bash -c 'ovsdb-client restore < /root/switch.db'
+      sleep 2
+      docker exec -itw /root ${container_name} bash -c 'rm /root/switch.db'
+    done
+
+    # Restore Datacenter Hosts
+    for dc in ${dchosts[@]}; do
+      for i in $(seq 1 4); do
+        cd $WORKDIR../students_config/
+
+        # Init host loc
+        data_center_loc='DCN'
+        if [[ $i -eq 4 ]]; then
+          data_center_loc='DCS'
         fi
 
-        # Ensure the kernel modules load first
-        modprobe mpls_router \
-                mpls_gso \
-                mpls_iptunnel \
-                openvswitch
+        hostname=${dc}_${i}
+        container_name=${as}_L2_${data_center_loc}_${hostname}
 
-        change_directory
+        echo "Restoring $container_name configuration..."
+        # Get the IPv4 address
+        ipv4=$(cat ${configs_folder_name}${hostname}/host.ip | grep -w inet | grep ${as}-S${i} | awk '{print $2}')
+        echo "Backuped $container_name IPv4: ${ipv4}"
+        # Get the IPv6 address
+        ipv6=$(cat ${configs_folder_name}${hostname}/host.ip | grep -w inet6 | grep ${as}-S${i} | awk '{print $2}')
+        echo "Backuped $container_name IPv6: ${ipv6}"
+        # Get default route (IPv4 only?)
+        default_route=$(cat ${configs_folder_name}${hostname}/host.route | grep -w default | awk '{print $3}')
+        echo "Backuped $container_name Default Route: ${default_route}"
 
-        # Restart containers
-        containerList=$(docker ps -a --format {{.Names}})
-        totalContainerCount=$(echo $containerList | wc -w)
-
-        currentContainer=1
-        for x in $containerList; do
-                echo "Starting container with Docker (${currentContainer}/${totalContainerCount}): "
-                echo -n "Container killed: "
-                docker kill $x
-                echo -n "Container started: "
-                docker start $x
-
-                (( currentContainer++ ))
-        done
-
-        # Reset IXP Containers
-        readarray groups < "${WORKDIR}"config/AS_config.txt
-        groupNums=${#groups[@]}
-
-        for ((k=0;k<groupNums;k++)); do
-                group_k=(${groups[$k]})
-                group_number="${group_k[0]}"
-                group_as="${group_k[1]}"
-                if [[ $group_as == "IXP" ]]; then
-                        echo "Resetting ${group_number}_IXP"
-                        docker rm -f "${group_number}_IXP"
-                        location="${WORKDIR}"groups/g"${group_number}"
-                        docker run -itd --net='none' --name="${group_number}""_IXP" \
-                                --pids-limit 200 --hostname "${group_number}""_IXP" \
-                                -v "${location}"/daemons:/etc/quagga/daemons \
-                                --privileged \
-                                --sysctl net.ipv4.ip_forward=1 \
-                                --sysctl net.ipv4.icmp_ratelimit=0 \
-                                --sysctl net.ipv4.fib_multipath_hash_policy=1 \
-                                --sysctl net.ipv4.conf.all.rp_filter=0 \
-                                --sysctl net.ipv4.conf.default.rp_filter=0 \
-                                --sysctl net.ipv4.conf.lo.rp_filter=0 \
-                                --sysctl net.ipv4.icmp_echo_ignore_broadcasts=0 \
-                                --sysctl net.ipv6.conf.all.disable_ipv6=0 \
-                                --sysctl net.ipv6.conf.all.forwarding=1 \
-                                --sysctl net.ipv6.icmp.ratelimit=0 \
-                                -v /etc/timezone:/etc/timezone:ro \
-                                -v /etc/localtime:/etc/localtime:ro \
-                                -v "${location}"/looking_glass.txt:/home/looking_glass.txt \
-                                --log-opt max-size=1m --log-opt max-file=3 \
-                                "miniinterneteth/d_ixp"
-                fi
-        done
-
-        for i in $(seq 1 2); do
-                echo "ITERATION: ${i} of 2"
-                currentContainer=1
-                for i in $containerList; do
-                        # Debug output
-                        echo "Calling restart_container.sh ${currentContainer} of ${totalContainerCount} : $i";
-                        # Then call ETHZurich's script
-                        ${WORKDIR}groups/restart_container.sh $i;
-
-                        (( currentContainer++ ))
-                done
-        done
-
-        add_vlans_l2
-        execute_ssh_port_forward
+        # Adding the IPv4 and IPv6 address
+        docker exec -itw /root ${container_name} ip address add ${ipv4} dev ${as}-S${i} &> /dev/null
+        docker exec -itw /root ${container_name} ip address add ${ipv6} dev ${as}-S${i} &> /dev/null
+        docker exec -itw /root ${container_name} ip route add default via ${default_route} &> /dev/null
+      done
+    done
+  done
 }
 
-soft_restart() {
-        change_directory
+show_passwords() {
+  cd $WORKDIR
+  echo "--- START OF ASes PASSWORDS ---"
+  cat groups/passwords.txt
+  echo "---  END OF ASes PASSWORDS  ---"
 
-        containers=$(docker ps -af 'status=exited' --format '{{.Names}}')
-        echo "Starting reported stopped containers: ${containers}"
-        for sC in $containers; do
-                echo "Container started: $(docker start $sC))"
-        done
+  echo "--- START OF krill_passwords ---"
+  cat groups/krill_passwords.txt
+  echo "---  END OF krill_passwords  ---"
 
-        echo "Calling restart_container.sh on all containers..."
-        containers=$(docker ps -a --format {{.Names}})
-        totalContainerCount=$(echo $containers | wc -w)
-
-        for i in $(seq 1 2); do
-                echo "ITERATION: ${i} of 2"
-                currentContainer=1
-                for c in $containers; do
-                        echo "Calling restart_container.sh on $c (${currentContainer} of ${totalContainerCount})"
-                        ${WORKDIR}groups/restart_container.sh $c
-
-                        (( currentContainer++ ))
-                done
-        done
-
-        add_vlans_l2
-        execute_ssh_port_forward
+  echo "--- START OF MEASUREMENT PASSWORDS ---"
+  cat groups/ssh_measurement.txt
+  echo "---  END OF MEASUREMENT PASSWORDS  ---"
 }
 
-echo "-- RESTART MINI INTERNET PROJECT --"
-echo "Script coded by Lab ICN F9.6 Team"
+show_help() {
+  echo "usage: $0 [options]"
+  echo "options available:"
+  echo -e "\t1\t save config to students_config directory (CAUTION: THE CONFIGS IN THIS DIRECTORY WILL BE OVERWRITTEN!)"
+  echo -e "\t2\t reset the mini internet and restore configs"
+}
 
-# Check if the script run as root
-if [[ $(id -u) -ne 0 ]]; then
-        echo "You must run as root, exiting..."
-        exit 1
-fi
+main() {
+  if [[ $(id -u) -ne 0 ]]; then
+    echo "You must run as root, exiting..."
+    exit 1
+  fi
 
-# Now check the selected argument
-cmd=$1
+  case $1 in
+    1)
+      save_configs
+      ;;
+    2)
+      reset_with_startup
+      restore_configs
+      echo "Restart complete, here are all passwords..."
+      show_passwords
+      ;;
+  esac
+}
 
-# If not empty, then check if hard or soft
-if [[ $cmd == "hard" ]]; then
-        echo "Hard restart selected"
-        hard_restart
-        exit $?
-elif [[ $cmd == "soft" ]]; then
-        echo "Soft restart selected"
-        soft_restart
-        exit $?
-else
-        echo "Usage: $0 [hard|soft]"
-        exit 1
-fi
+welcome() {
+  cmd=$1
+  if [ $cmd ]; then
+    if [ $cmd -eq 1 ] || [ $cmd -eq 2 ]; then
+      main $cmd
+    else
+      echo -e "invalid options"
+      show_help
+    fi
+  else
+    show_help
+  fi
+}
+
+welcome $@
